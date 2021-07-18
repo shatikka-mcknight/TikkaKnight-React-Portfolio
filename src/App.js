8ebdd71a80ba4c71deb757b7e4dce0e6ae7aa3cb
Shatikka McKnight <32470118+shatikka-mcknight@users.noreply.github.com>
2021-07-18 14:05:27 -0400

    update README file with project breakdown

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../src/components/Header';
-import Navigation from '../src/components/Navigation';
 import Project from '../src/components/Project';
 import Footer from '../src/components/Footer';import './App.css';
 
@@ -11,7 +10,6 @@ function App() {
     <div>
     <div className="flex-column justify-flex-start min-100-vh">
       <Header />
-            
     </div>
     <div className="flex-column justify-flex-start min-100-vh">
     <div>
